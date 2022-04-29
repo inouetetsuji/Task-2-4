ecdca9eb5676ead7cc9d1db7d364f347252cf32a
inouetetsuji <81937796+inouetetsuji@users.noreply.github.com>
2022-04-29 17:31:18 +0900

    Add files via upload

---
+++ study/Study.java
@@ -0,0 +1,18 @@
+package study;
+
+public class Study {
+
+  private static final String FRUITS_LEMON_04 = null;
+  private static final String FRUITS_COUNT_20 = null;
+
+public static void main(String[] args) {
+
+      /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+       *     また、課題の画像と同じ表示になるように
+       *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
+       */
+	
+	   String fruits=constants.FruitsConstants.FRUITS_LEMON_04;
+	   int count=constants.FruitsConstants.FRUITS_COUNT_20;
+}
+}
